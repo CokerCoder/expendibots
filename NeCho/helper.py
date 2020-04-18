0665242c26ee0047907f5a53483c98c5767672cc
Yunfei Jing <jinyj@student.unimelb.edu.au>
2020-04-18 15:34:19 +1000

    final data structure update

--- NeCho/helper.py
+++ NeCho/helper.py
@@ -1,12 +1,6 @@
 import math
 
 
-class Piece:
-    def __init__(self, colour, n):
-        self.colour = colour
-        self.n = n
-
-
 # Calculate the euclidean distance between 2 tokens, given as (x, y) format.
 def euclidean(pos1, pos2):
     distance = math.sqrt((pos1[-2] - pos2[-2]) ** 2 + (pos1[-1] - pos2[-1]) ** 2)
@@ -31,87 +25,61 @@ def explosion_range(pos):
 
 
 # Explosion function, will affect the surrounding pieces and recursively explode
-def boom(board, pos, black, white):
-    if board[pos[0]][pos[1]] is None:
+def boom(state, pos):
+    black = [tuple(b[1:]) for b in state["black"]]
+    white = [tuple(w[1:]) for w in state["white"]]
+    if pos not in black and pos not in white:
         return
-    remove_piece(board, pos, black, white)
+    if pos in black:
+        del state["black"][black.index(pos)]
+    else:
+        del state["white"][white.index(pos)]
+
     affected = explosion_range(pos)
     for affected_piece in affected:
-        boom(board, affected_piece, black, white)
-
-
-# Helper function to remove the affected pieces from the board and the black and white lists respectively
-def remove_piece(board, pos, black, white):
-    colour = board[pos[0]][pos[1]].colour
-    if colour == 'black':
-        black.remove((pos[0], pos[1]))
-    elif colour == 'white':
-        white.remove((pos[0], pos[1]))
-    board[pos[0]][pos[1]] = None
+        boom(state, affected_piece)
 
 
 # Move n white/black tokens from old_pos to new_pos
-def move(board, n, old_pos, new_pos, colour, pieces):
-    if old_pos == new_pos:
-        return
+def move(state, n, old_pos, new_pos, colour):
+    pieces = [tuple(p[1:]) for p in state[colour]]
 
-    (old_x, old_y) = old_pos
-    (new_x, new_y) = new_pos
+    # add n to new position
+    if new_pos in pieces:
+        new_index = pieces.index(new_pos)
+        state[colour][new_index][0] += n
+    elif new_pos not in pieces:
+        state[colour].append([n, new_pos[0], new_pos[1]])
 
-    board[old_x][old_y].n -= n
-    if board[old_x][old_y].n == 0:
-        pieces.remove(old_pos)
-        board[old_x][old_y] = None
-
-    if board[new_x][new_y] is not None:
-        board[new_x][new_y].n += n
-    elif board[new_x][new_y] is None:
-        board[new_x][new_y] = Piece(colour, n)
-
-    if new_pos not in pieces:
-        pieces.append(new_pos)
+    # remove n from old position
+    old_index = pieces.index(old_pos)
+    state[colour][old_index][0] -= n
+    if state[colour][old_index][0] == 0:
+        del state[colour][old_index]
 
 
 # Self implement function to list all the possible moves of a given piece
-def available_actions(board, colour, pieces):
+def available_actions(state, colour):
+    black = [tuple(b[1:]) for b in state["black"]]
+    white = [tuple(w[1:]) for w in state["white"]]
     actions = []
-    for (x, y) in pieces:
+    for [n, x, y] in state[colour]:
         actions.append(("BOOM", (x, y)))
-        stack = board[x][y].n
-        for d in range(1, stack + 1):
+        for d in range(1, n + 1):
             possible_moves = [(x + d, y), (x, y + d), (x - d, y), (x, y - d)]
             for (new_x, new_y) in possible_moves:
-                if check_availability((new_x, new_y)) and \
-                        ((board[new_x][new_y] is None) or board[new_x][new_y].colour == colour):
+                if check_availability((new_x, new_y)) and (
+                    False if (colour == "black" and (new_x, new_y) in white)
+                    else False if (colour == "white" and (new_x, new_y) in black)
+                    else True
+                ):
                     actions.append(("MOVE", d, (x, y), (new_x, new_y)))
     return actions
 
 
 # Evaluate function to calculate current board state for a player
-# Ver 1.1
-def evaluate(board, colour, black, white):
-    # points for black, return -points for white player
-    points = 0
-
-    # A stack of pieces worth much more than a single piece
-    # In general, a stack of N pieces worth 4^N points than a single piece which worth only 4
-    # The total points is the sum of the current player's pieces points minus the one of the opponent
-    for row in range(8):
-        for col in range(8):
-            if board[row][col] is None:
-                continue
-            stack = board[row][col].n
-            if board[row][col].colour == colour:
-                points += 4 ** stack
-            else:
-                points -= 4 ** stack
-
-    # Consider the distance, calculate the total average distance between each black and white
-    # Aggressive, minimize the distance
-    dis = []
-    for black_piece in black:
-        for white_piece in white:
-            dis.append(euclidean(black_piece, white_piece))
-    if len(dis) > 0:
-        points -= 10 * (sum(dis) / len(dis))
-    return points
+# Ver 1.1.1
+def evaluate(state, colour):
+    points = len(state["black"]) - len(state["white"])
+    # Simply return the difference of their number of tokens
+    return points if colour == "black" else -points
--- NeCho/player.py
+++ NeCho/player.py
@@ -8,49 +8,46 @@ class ExamplePlayer:
     def __init__(self, colour):
 
         self.colour = colour
-
-        self.board = \
-            [[Piece("black", 1) if ((y == 6 or y == 7) and (x != 2 and x != 5))
-              else Piece("white", 1) if ((y == 0 or y == 1) and (x != 2 and x != 5))
-            else None for y in range(8)] for x in range(8)]
-
-        self.black = [(0, 7), (1, 7), (3, 7), (4, 7), (6, 7), (7, 7), (0, 6), (1, 6), (3, 6), (4, 6), (6, 6), (7, 6)]
-        self.white = [(0, 1), (1, 1), (3, 1), (4, 1), (6, 1), (7, 1), (0, 0), (1, 0), (3, 0), (4, 0), (6, 0), (7, 0)]
+        self.state = {
+            "black": [[1, 0, 7], [1, 1, 7], [1, 3, 7], [1, 4, 7], [1, 6, 7], [1, 7, 7], [1, 0, 6], [1, 1, 6], [1, 3, 6],
+                      [1, 4, 6], [1, 6, 6], [1, 7, 6]],
+            "white": [[1, 0, 1], [1, 1, 1], [1, 3, 1], [1, 4, 1], [1, 6, 1], [1, 7, 1], [1, 0, 0], [1, 1, 0], [1, 3, 0],
+                      [1, 4, 0], [1, 6, 0], [1, 7, 0]]
+        }
 
     def action(self):
 
-        possible_actions = available_actions(self.board, self.colour, getattr(self, self.colour))
+        possible_actions = available_actions(self.state, self.colour)
 
-        # Action Ver 0: Choose random move
-        # return possible_actions[randrange(0, len(possible_actions))]
+        #
+        # # Action Ver 0: Choose random move
+        # # return possible_actions[randrange(0, len(possible_actions))]
+        #
 
-        # Action Ver 1: Choose the move which benefit the most, consider only one move further
+        # # Action Ver 1: Greedy method
         max_point = -9999
         max_action = None
         for possible_action in possible_actions:
 
             # In the case using a 2D array, deepcopy must be used since a 2D array contains objects and we want the
             # value not by its reference pointer so that we can modify without affecting the original one
-            # but this method is slow...
-            candidate_board = deepcopy(self)
+            candidate_board = deepcopy(self.state)
 
             if possible_action[0] == 'BOOM':
-                boom(candidate_board.board, possible_action[1], candidate_board.black, candidate_board.white)
+                boom(candidate_board, possible_action[1])
             elif possible_action[0] == 'MOVE':
-                move(candidate_board.board, possible_action[1], possible_action[2], possible_action[3], self.colour,
-                     getattr(candidate_board, self.colour))
+                move(candidate_board, possible_action[1], possible_action[2], possible_action[3], self.colour)
 
             # calculate
-            point = evaluate(candidate_board.board, self.colour, candidate_board.black, candidate_board.white)
+            point = evaluate(candidate_board, self.colour)
             if point > max_point:
                 max_point = point
                 max_action = possible_action
 
-        return max_action if max_action else possible_actions[0]
+        return max_action
 
     def update(self, colour, action):
-
         if action[0] == 'BOOM':
-            boom(self.board, action[1], self.black, self.white)
+            boom(self.state, action[1])
         elif action[0] == 'MOVE':
-            move(self.board, action[1], action[2], action[3], colour, getattr(self, colour))
+            move(self.state, action[1], action[2], action[3], colour)
